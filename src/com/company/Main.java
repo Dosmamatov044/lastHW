ef8802c68efa63fd9e10233c313c518a43396ff6
Dosmamatov044 <dosmamatov24@gmail.com>
2020-03-21 22:27:47 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,62 @@
+package com.company;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Map<String, String[]> dictionary = new HashMap<>();
+        Map<String, String[]> newWords = new HashMap<>();
+
+        dictionary.put("Глупый", new String[]{"Немой", "Похой", "НЕнормальный", "Сумасшедший","Тупенький"});
+        dictionary.put("Животное", new String[]{"Существо", "Млекопитающий", "Дикарь", "Зверек"});
+        dictionary.put("Дом ", new String[]{"жилище", "помещение", "логово", "кров"});
+        dictionary.put("Тратить", new String[]{"Одолжит", "Продать", "Дават в взаимы"});
+        dictionary.put("Я", new String[]{"Сам", "Себе", "Мой", "Моя"});
+        dictionary.put("Он", new String[]{"Она", "Она", "Они"});
+        dictionary.put("Думать", new String[]{"Мыслить", "Задумыватся", "предпологать", "ощущать"});
+        dictionary.put("Компьютер", new String[]{"Ноутбук", "Телефон", "Гаджет", "Планшет"});
+        dictionary.put("Обычная Вода", new String[]{"Холодная вода", "Теплая вода", "Заморожонная вода", "Кипящая вода"});
+        dictionary.put("Летать", new String[]{" Прыгать", " Скакать", "Бегать", "Двигаться"});
+        dictionary.put("Мечта", new String[]{"Сон", "Dream"});
+
+        Set<String> keys = dictionary.keySet();
+        Iterator iterator = keys.iterator();
+        while(iterator.hasNext()){
+            String key = iterator.next().toString();
+            String[] values = dictionary.get(key);
+            newWords.put(key, values);
+            for (int i = 0; i <values.length ; i++) {
+                String StillKeys = values[i];
+                ArrayList<String> common = new ArrayList<>(values.length);
+                common.addAll(Arrays.asList(values));
+                common.remove(StillKeys);
+                common .add(key);
+                String[] StillValues = new String[common.size()];
+                StillValues =common .toArray(StillValues);
+                System.out.println(StillKeys + " - " + Arrays.toString(StillValues));
+                newWords.put(StillKeys, StillValues);
+            }
+        }
+     boolean i=true;
+        while (i) {
+            System.out.println("Введите слово: ");
+            Scanner scanner = new Scanner(System.in);
+            String sentences = scanner.nextLine();
+            String[] words = sentences.split(" ");
+            for (String word : words) {
+                String[] synonyms = dictionary.get(word);
+                String[] Synonyms1 = newWords.get(word);
+                Random r = new Random();
+                if (synonyms != null) {
+                    System.out.print(synonyms[r.nextInt(synonyms.length)] + " ");
+                } if (Synonyms1 !=null){
+                    System.out.print(Synonyms1[r.nextInt(Synonyms1.length)] + " ");
+                } else {
+                    System.out.print("не существут в списке");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
